87d83c9cb554e68d1198351b7e673e2e4e337632
Guy Davidson <guydav12@gmail.com>
2021-01-12 19:25:37 +0200

    init array done

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function updatearraySizeSliderPick(val) {
-          document.getElementById('arraySizeSliderPick').value=val;
-        }
+function updatearraySizeSliderPick(size) {
+  // update selected value to slider view button:
+  document.getElementById('arraySizeSliderPick').value = size;
+
+  //selcet view port and create 'size' bars:
+  let view = document.querySelector('.view');
+  view.innerHTML = "";
+
+  for (let i = 0; i < size; i++) {
+
+    const newDiv = document.createElement("div");
+    newDiv.className = "elem" + i;
+
+    const newDivSpan = document.createElement("span");
+    let barVal = Math.floor((Math.random() * 100) + 1);
+    let t = document.createTextNode(barVal);
+    newDiv.style.width = barVal + "%";
+    newDivSpan.appendChild(t);
+    newDiv.appendChild(newDivSpan);
+
+    view.appendChild(newDiv);
+  }
+}
