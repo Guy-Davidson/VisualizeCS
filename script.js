646b9ecb5967279d4c299e24af7e1e8893adb569
Guy Davidson <guydav12@gmail.com>
2021-01-12 20:41:39 +0200

    only sort left

--- script.js
+++ script.js
@@ -14,10 +14,71 @@ function updatearraySizeSliderPick(size) {
     const newDivSpan = document.createElement("span");
     let barVal = Math.floor((Math.random() * 100) + 1);
     let t = document.createTextNode(barVal);
+    newDivSpan.style.color = arrayBarNumberColor(barVal);
     newDiv.style.width = barVal + "%";
+    newDiv.style.backgroundColor = arrayBarColor(barVal);
     newDivSpan.appendChild(t);
     newDiv.appendChild(newDivSpan);
 
     view.appendChild(newDiv);
+
   }
+}
+
+function arrayBarColor(barVal) {
+  if (barVal < 10) {
+    return "#041D61";
+  } else if (barVal < 20) {
+    return "#2C306E";
+  } else if (barVal < 30) {
+    return "#12257A";
+  } else if (barVal < 40) {
+    return "#1A88C7";
+  } else if (barVal < 50) {
+    return "#4AA6CF";
+  } else if (barVal < 60) {
+    return "#13B9DB";
+  } else if (barVal < 70) {
+    return "#2ACAE6";
+  } else if (barVal < 80) {
+    return "#7EE5D9";
+  } else if (barVal < 90) {
+    return "#FFE3BC";
+  } else {
+    return "#E6FEFE";
+  }
+}
+
+function arrayBarNumberColor(barVal) {
+  if (barVal < 50) {
+    return "#E6FEFE";
+  }
+  return "#0d1117";
+}
+
+function reset() {
+  document.getElementById('arraySizeSliderPick').value = 0;
+
+  let view = document.querySelector('.view');
+  view.innerHTML = "";
+}
+
+function bubbleSortSelected(_this) {
+  _this.style.backgroundColor = "#F0883E";
+}
+
+function notbubbleSortSelected(_this) {
+  let sortTypePort = document.querySelector('.sortType');
+  let lastSpan = sortTypePort.querySelector("span");
+  if (lastSpan) {
+    sortTypePort.removeChild(lastSpan);
+  }
+  const newSpan = document.createElement("span");
+  const t = document.createTextNode("currently only bubble sort is implemented.");
+  newSpan.appendChild(t);
+  sortTypePort.appendChild(newSpan);
+}
+
+function sort() {
+
 }
