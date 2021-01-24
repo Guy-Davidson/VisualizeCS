d4812b0fe54db012224c3ab409b498e6d49fb8f9
Guy Davidson <guydav12@gmail.com>
2021-01-24 15:22:07 +0200

    maze race is set

--- myPathFinder.js
+++ myPathFinder.js
@@ -1,4 +1,31 @@
-// Global Variables:
+// My Maze Path Finding :
+
+//Constants:
+const MIN_MAZE_SIZE = 50;
+const MID_MAZE_SIZE = 75;
+const MAX_MAZE_SIZE = 100;
+
+const MIN_FINDING_DELAY = 2;
+const MAX_FINDING_DELAY = 5;
+
+const DFS_ALG = 1;
+const BFS_ALG = 2;
+const MAZE_RACE = 3;
+
+const MAZE_START = 1;
+const MAZE_FINISH = MAX_MAZE_SIZE ** 2;
+
+
+//Global vars:
+let mazeSize = MAX_MAZE_SIZE;
+let findingDelay = MAX_FINDING_DELAY;
+let findingAlg = DFS_ALG;
+let startingPoint = MAZE_START;
+let finishPoint = MAZE_FINISH;
+let isFirstToFind = 1;
+let selectedBtnMaze = RACE;
+
+
 // union-find arrays:
 let cells = [];
 let components = [];
@@ -9,13 +36,64 @@ let edges = [];
 
 //Classes:
 class Vertex {
-  constructor(div, adjList = []) {
+  constructor(div, adjList = [], isFinish = false, isDFSMarked = false, isBFSMarked = false) {
     this.div = div;
     this.adjList = adjList;
+    this.isFinish = isFinish;
+    this.isDFSMarked = isDFSMarked;
+    this.isBFSMarked = isBFSMarked;
   }
 }
 
-function generateMaze(size, viewPort) {
+async function mazeRace(viewPort) {
+  mazeRaceSelected(document.getElementById('mazeRaceBtn'))
+  document.getElementById('mazeResetBtn').click();
+
+  // count down:
+  let view = document.querySelector('.' + viewPort);
+  let counter = document.createElement("h1");
+  let t = document.createTextNode("3");
+  counter.style.fontSize = "9rem";
+  view.style.textAlign = "center";
+  counter.appendChild(t);
+  view.appendChild(counter);
+
+  for (let k = 1; k < 4; k++) {
+    setTimeout(() => {
+      counter.textContent = 3 - k;
+    }, k * 1000);
+  }
+
+  await sleep(4000);
+
+  //Set all clicks to start a race:
+  document.getElementById('mazeResetBtn').click();
+  generateMaze(viewPort);
+  document.getElementById('dfsBtn').click();
+  document.getElementById('findPathBtn').click();
+  document.getElementById('bfsBtn').click();
+  document.getElementById('findPathBtn').click();
+  mazeRaceSelected(document.getElementById('mazeRaceBtn'))
+}
+
+function findPath() {
+  if (findingAlg == DFS_ALG) {
+    DFS(vertices[startingPoint]);
+  } else {
+    BFS(vertices[startingPoint]);
+  }
+  nextStartingPoint();
+}
+
+function nextStartingPoint() {
+  if (startingPoint == MAZE_START) {
+    startingPoint = mazeSize;
+  }
+}
+
+function generateMaze(viewPort) {
+  mazeReset(viewPort);
+  let size = mazeSize;
 
   generateMazeGrid(size, viewPort);
 
@@ -48,10 +126,17 @@ function generateMaze(size, viewPort) {
       vertices[cell2].adjList.push(vertices[cell1]);
     }
   }
+  generateFinishPoint(size);
+}
+
+function generateFinishPoint(size) {
+  let r = Math.floor((Math.random() * (size ** 2)) + 1);
+  vertices[r].isFinish = true;
+  let div = vertices[r].div
+  div.style.backgroundColor = "yellow";
 }
 
 function generateMazeGrid(size, viewPort) {
-  document.getElementById('mazeSizeSlider').value = size;
 
   let view = document.querySelector('.' + viewPort);
   view.style.display = "grid";
@@ -118,10 +203,167 @@ async function DFS(vertex) {
   stack.push(vertex);
   while (stack.length > 0) {
     let v = stack.pop();
-    if (!v.div.classList.contains("dfsMarked")) {
-      v.div.classList.add("dfsMarked");
-      v.adjList.forEach(v => stack.push(v));
-      await sleep(5);
+    if (v.isFinish == true) {
+      return;
     }
+    if (!v.isDFSMarked) {
+      v.isDFSMarked = true;
+      v.div.classList.add("dfsMarked");
+      v.div.classList.remove("bfsMarked");
+      v.adjList.forEach(v => stack.push(v));
+    }
+    await sleep(findingDelay);
   }
+}
+
+async function BFS(vertex) {
+  let queue = [];
+  vertex.div.classList.add("bfsMarked");
+  queue.push(vertex);
+  while (queue.length > 0) {
+    let v = queue.shift();
+    if (v.isFinish == true) {
+      return;
+    }
+    v.adjList.forEach(u => {
+      if (!u.isBFSMarked) {
+        v.isBFSMarked = true;
+        u.div.classList.add("bfsMarked");
+        u.div.classList.remove("dfsMarked");
+        queue.push(u);
+      }
+    })
+    await sleep(findingDelay);
+  }
+}
+
+function mazeReset(viewPort) {
+  let view = document.querySelector('.' + viewPort);
+  view.innerHTML = "";
+
+  cells = [];
+  components = [];
+
+  vertices = [1];
+  edges = [];
+  startingPoint = MAZE_START;
+}
+
+function dfsSelected(_this) {
+  _this.classList.toggle("selectedButton");
+
+  let b1 = document.getElementById("bfsBtn");
+  if (b1.classList.contains("selectedButton")) {
+    b1.classList.remove("selectedButton");
+  }
+
+  let b2 = document.getElementById("mazeRaceBtn");
+  if (b2.classList.contains("selectedButton")) {
+    b2.classList.remove("selectedButton");
+  }
+
+  findingAlg = DFS_ALG;
+}
+
+function bfsSelected(_this) {
+  _this.classList.toggle("selectedButton");
+
+  let b1 = document.getElementById("dfsBtn");
+  if (b1.classList.contains("selectedButton")) {
+    b1.classList.remove("selectedButton");
+  }
+
+  let b2 = document.getElementById("mazeRaceBtn");
+  if (b2.classList.contains("selectedButton")) {
+    b2.classList.remove("selectedButton");
+  }
+
+  findingAlg = BFS_ALG;
+}
+
+function mazeRaceSelected(_this) {
+  _this.classList.toggle("selectedButton");
+
+  let b1 = document.getElementById("bfsBtn");
+  if (b1.classList.contains("selectedButton")) {
+    b1.classList.remove("selectedButton");
+  }
+
+  let b2 = document.getElementById("dfsBtn");
+  if (b2.classList.contains("selectedButton")) {
+    b2.classList.remove("selectedButton");
+  }
+
+  sortingAlg = QUICK;
+  selectedBtn = QUICK;
+}
+
+function smallMazeSelected(_this) {
+  _this.classList.toggle("selectedButton");
+
+  let b1 = document.getElementById("mediumMaze");
+  if (b1.classList.contains("selectedButton")) {
+    b1.classList.remove("selectedButton");
+  }
+
+  let b2 = document.getElementById("largeMaze");
+  if (b2.classList.contains("selectedButton")) {
+    b2.classList.remove("selectedButton");
+  }
+
+  mazeSize = MIN_MAZE_SIZE;
+}
+
+function mediumMazeSelected(_this) {
+  _this.classList.toggle("selectedButton");
+
+  let b1 = document.getElementById("smallMaze");
+  if (b1.classList.contains("selectedButton")) {
+    b1.classList.remove("selectedButton");
+  }
+
+  let b2 = document.getElementById("largeMaze");
+  if (b2.classList.contains("selectedButton")) {
+    b2.classList.remove("selectedButton");
+  }
+
+  mazeSize = MID_MAZE_SIZE;
+}
+
+function largeMazeSelected(_this) {
+  _this.classList.toggle("selectedButton");
+
+  let b1 = document.getElementById("smallMaze");
+  if (b1.classList.contains("selectedButton")) {
+    b1.classList.remove("selectedButton");
+  }
+
+  let b2 = document.getElementById("mediumMaze");
+  if (b2.classList.contains("selectedButton")) {
+    b2.classList.remove("selectedButton");
+  }
+
+  mazeSize = MAX_MAZE_SIZE;
+}
+
+function slowSpeedSelected(_this) {
+  _this.classList.toggle("selectedButton");
+
+  let b1 = document.getElementById("fastSpeedBtn");
+  if (b1.classList.contains("selectedButton")) {
+    b1.classList.remove("selectedButton");
+  }
+
+  findingDelay = MAX_FINDING_DELAY;
+}
+
+function fastSpeedSelected(_this) {
+  _this.classList.toggle("selectedButton");
+
+  let b1 = document.getElementById("slowSpeedBtn");
+  if (b1.classList.contains("selectedButton")) {
+    b1.classList.remove("selectedButton");
+  }
+
+  findingDelay = MIN_FINDING_DELAY;
 }
