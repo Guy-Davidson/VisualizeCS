9015c18a572375e83c9ad29ffa20277b10724e1f
Guy laptop <guydav12@gmail.com>
2021-02-16 00:07:29 +0200

    screen fit

--- myPathFinder.js
+++ myPathFinder.js
@@ -49,7 +49,7 @@ class Vertex {
 
 //Dynamicly Set highet to modules:
 window.addEventListener('load', (event) => {
-  let h = window.innerHeight * 0.80;
+  let h = window.innerHeight * 0.82;  
   document.querySelector(".maze").style.height = h + "px";
   document.querySelector(".sort").style.height = h + "px";
 })
